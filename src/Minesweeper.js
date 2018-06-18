b3db4efe13c6d618f24499e9d0581b01f2f7a236
Domenic Rocca <rocca.domenic@gmail.com>
2018-06-18 12:47:44 -0400

    Part 1 Complete

--- src/Minesweeper.js
+++ src/Minesweeper.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const blankline = '  |   |  ';
+const guessline = '1 |   |  ';
+const bombline = '  | B |  ';
+console.log('This is what an empty board would look like:');
+console.log(blankline);
+console.log(blankline);
+console.log(blankline);
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessline);
+console.log(bombline);
+console.log(blankline);
